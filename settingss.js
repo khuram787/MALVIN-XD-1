cd7d392d4eba0054da3fba769ddc19a68bb1eae9
khuram787 <khuramssultan@gmail.com>
2025-08-16 01:40:05 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~pulyXLjQ#yBdZZ8_3LnxGBGM_GUP-NKxkrUeFH7piQcgmzzmp0h4",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
